6c6a30f87a2d168e63e53e9ea2273986032a39d2
yTsunomoto <wasiwasimaru@gmail.com>
2020-07-16 22:38:15 +0900

    add 新規作成。

---
+++ DotNetCoreMagickNet/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DotNetCoreMagickNet.Models;
+using ImageMagick;
+using Microsoft.Extensions.Hosting;
+
+namespace DotNetCoreMagickNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string _rootPath;
+
+        public HomeController(ILogger<HomeController> logger, IHostEnvironment hostEnvironment)
+        {
+            _logger = logger;
+            _rootPath = hostEnvironment.ContentRootPath;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Download()
+        {
+            _logger.LogInformation("start create image");
+
+            MagickNET.SetGhostscriptDirectory(_rootPath);
+            var settings = new MagickReadSettings { Density = new Density(300, 300) };
+
+            byte[] img;
+            using (var images = new MagickImageCollection())
+            {
+                images.Read(Path.Combine(_rootPath, "sample.pdf"), settings);
+                img = images.First().ToByteArray(MagickFormat.Png);
+            }
+            return File(img, MediaTypeNames.Application.Octet, "sample.png");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# dotnet-core-magick-net
+Magick.NET と Ghostscript を用いて PDF からイメージ画像を出力するサンプル
+
+## Feature
+- .NET Core 3.1
+- ASP.NET Core
+- Magick.NET
+
+# Usage
+- ナビバー > Download より PDF から出力した画像を作成し、ダウンロードします。
